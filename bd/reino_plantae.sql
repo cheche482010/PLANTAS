0ef22a516f6801277851906d0301566328acb8f2
cheche482010 <cheche482010@gmail.com>
2023-03-20 10:55:09 -0400

    correccion bd

--- bd/reino_plantae.sql
+++ bd/reino_plantae.sql
@@ -1,22 +1,22 @@
 CREATE TABLE plantas (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nombre_comun TEXT,
     nombre_cientifico TEXT,
     descripcion TEXT
 );
 CREATE TABLE caracteristicas (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nombre TEXT
 );
 CREATE TABLE relaciones (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     planta_id INTEGER,
     caracteristica_id INTEGER,
     FOREIGN KEY(planta_id) REFERENCES plantas(id),
     FOREIGN KEY(caracteristica_id) REFERENCES caracteristicas(id)
 );
 CREATE TABLE habitats (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     nombre TEXT,
     descripcion TEXT
 );
